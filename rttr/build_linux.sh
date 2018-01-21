df0d2a026e9487f53f6b2b63a9e5df938753d9e5
Bas Korsmit <bmodone@gmail.com>
2018-01-20 17:56:39 -0800

    Updating linux install

--- rttr/build_linux.sh
+++ rttr/build_linux.sh
@@ -4,5 +4,5 @@ cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P
 mkdir linux
 cmake -H. -Blinux -DCMAKE_INSTALL_PATH=./linux/install
 cd linux
-cmake --build . --target install --config Release
-cmake --build . --target install --config Debug
+cmake --build . --target install --config Release -- -j 8
+cmake --build . --target install --config Debug -- -j 8
