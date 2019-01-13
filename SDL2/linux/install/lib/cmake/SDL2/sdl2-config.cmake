cdb5afccaabab6a752c3a7a3ee687dbe8120b779
cklosters <coen.klosters@gmail.com>
2019-01-13 18:52:23 +0100

    Linux SDL 2.0.9 update

--- SDL2/linux/install/lib/cmake/SDL2/sdl2-config.cmake
+++ SDL2/linux/install/lib/cmake/SDL2/sdl2-config.cmake
@@ -1,11 +1,11 @@
 # sdl2 cmake project-config input for ./configure scripts
 
-set(prefix "/home/creywood/workspaces/naivi/thirdpartybuild/SDL2-2.0.8/linux/install") 
+set(prefix "/home/cklosters/dev/SDL2-2.0.9/build/home/cklosters/dev/thirdparty/SDL2/linux/install") 
 set(exec_prefix "${prefix}")
 set(libdir "${exec_prefix}/lib")
-set(SDL2_PREFIX "/home/creywood/workspaces/naivi/thirdpartybuild/SDL2-2.0.8/linux/install")
-set(SDL2_EXEC_PREFIX "/home/creywood/workspaces/naivi/thirdpartybuild/SDL2-2.0.8/linux/install")
+set(SDL2_PREFIX "/home/cklosters/dev/SDL2-2.0.9/build/home/cklosters/dev/thirdparty/SDL2/linux/install")
+set(SDL2_EXEC_PREFIX "/home/cklosters/dev/SDL2-2.0.9/build/home/cklosters/dev/thirdparty/SDL2/linux/install")
 set(SDL2_LIBDIR "${exec_prefix}/lib")
 set(SDL2_INCLUDE_DIRS "${prefix}/include/SDL2")
-set(SDL2_LIBRARIES "-L${SDL2_LIBDIR} -Wl,-rpath,${libdir} -Wl,--enable-new-dtags -lSDL2 ")
+set(SDL2_LIBRARIES "-L${SDL2_LIBDIR} -Wl,-rpath,${libdir} -Wl,--enable-new-dtags -lSDL2")
 string(STRIP "${SDL2_LIBRARIES}" SDL2_LIBRARIES)
