f7598086c6aace475e2e596b2d2e9ec30271dd34
Chris Heywood <31988069+cheywood@users.noreply.github.com>
2021-10-13 18:57:15 +0930

    SPIRV-Cross Linux ARM, reorganise

--- SPIRV-cross/make_install_osx.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-mkdir build
-cd build
-cmake ../ -DCMAKE_INSTALL_PREFIX="../osx/install" -DSPIRV_CROSS_FORCE_PIC=ON
-cmake --build . --config Release --target install
-cmake --build . --config Debug --target install
