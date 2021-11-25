cdd07ab1a7a34192c1baed662e6e5f5163f9573e
Coen Klosters <coen@naivi.nl>
2021-11-25 12:44:21 +0100

    updating vulkanSDK to 1.2.189 macOS x86_64

--- vulkansdk/macos/x86_64/lib/cmake/SPIRV-Tools/SPIRV-ToolsTarget-release.cmake
+++ vulkansdk/macos/x86_64/lib/cmake/SPIRV-Tools/SPIRV-ToolsTarget-release.cmake
@@ -5,15 +5,15 @@
 # Commands may need to know the format version.
 set(CMAKE_IMPORT_FILE_VERSION 1)
 
-# Import target "SPIRV-Tools" for configuration "Release"
-set_property(TARGET SPIRV-Tools APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
-set_target_properties(SPIRV-Tools PROPERTIES
+# Import target "SPIRV-Tools-static" for configuration "Release"
+set_property(TARGET SPIRV-Tools-static APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(SPIRV-Tools-static PROPERTIES
   IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
   IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libSPIRV-Tools.a"
   )
 
-list(APPEND _IMPORT_CHECK_TARGETS SPIRV-Tools )
-list(APPEND _IMPORT_CHECK_FILES_FOR_SPIRV-Tools "${_IMPORT_PREFIX}/lib/libSPIRV-Tools.a" )
+list(APPEND _IMPORT_CHECK_TARGETS SPIRV-Tools-static )
+list(APPEND _IMPORT_CHECK_FILES_FOR_SPIRV-Tools-static "${_IMPORT_PREFIX}/lib/libSPIRV-Tools.a" )
 
 # Import target "SPIRV-Tools-shared" for configuration "Release"
 set_property(TARGET SPIRV-Tools-shared APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
